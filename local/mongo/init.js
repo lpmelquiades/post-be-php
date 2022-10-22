1aa93251f305c1ddd353e44c1cfe81ac3d370127
Lucas Melquiades <lmelquiades@primeit.pt>
2022-10-22 12:56:49 -0300

    add indexes to db init

--- local/mongo/init.js
+++ local/mongo/init.js
@@ -4,12 +4,87 @@ db.createCollection('post');
 
 db.post.createIndex(
     {
-        "ticket_begin": 1,
-        "ticket_end": 1,
-        "ticket_count": 1,
-        "user_name": 1
+        "user_name": 1,
+        "ticket_begin": -1,
+        "ticket_end": -1,
+        "ticket_count": 1
     },
     {
         unique: true
+    }
+);
+
+db.post.createIndex(
+    {
+        "id": 1
+    },
+    {
+        unique: true
+    }
+);
+
+db.post.createIndex(
+    {
+        "user_name": 1
+    },
+    {
+        unique: false
+    }
+);
+
+db.post.createIndex(
+    {
+        "ticket_begin": -1
+    },
+    {
+        unique: false
+    }
+);
+
+db.post.createIndex(
+    {
+        "ticket_end": -1
+    },
+    {
+        unique: false
+    }
+);
+
+db.post.createIndex(
+    {
+        "user_name": 1,
+        "ticket_begin": -1
+    },
+    {
+        unique: false
+    }
+);
+
+db.post.createIndex(
+    {
+        "user_name": 1,
+        "ticket_end": -1
+    },
+    {
+        unique: false
+    }
+);
+
+db.post.createIndex(
+    {
+        "ticket_begin": -1,
+        "ticket_end": -1
+    },
+    {
+        unique: false
+    }
+);
+
+db.post.createIndex(
+    {
+        "created_at": -1
+    },
+    {
+        unique: false
     }
 );
